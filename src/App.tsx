0420fa64ccf10b20d7068508c18804bb159899ee
JonashGana <jonashtiglao@gmail.com>
2024-10-30 22:31:39 +0800

    added imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import ResumeBuilder from './components/ResumeBuilder'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-      <h1 className=' text-slate-300'>asfaf</h1>
-    </>
+    <div className='px-12 pt-12 bg-gray-200 w-full min-h-screen'>
+      <h1 className=' text-5xl font-bold pb-2'>Resume Builder</h1>
+      <p className=' text-slate-600'>Create your own brand new resume in less than 5 minutes.</p>
+      <ResumeBuilder />
+      <Footer />
+    </div>
   )
 }
 
