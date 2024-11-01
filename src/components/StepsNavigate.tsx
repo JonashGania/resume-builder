f34dda0f7ceac9e8188257b93122004d88290a50
JonashGana <jonashtiglao@gmail.com>
2024-11-01 23:10:00 +0800

    no change

--- src/components/StepsNavigate.tsx
+++ src/components/StepsNavigate.tsx
@@ -1,8 +1,30 @@
-const StepsNavigate = () => {
+import React from "react";
+
+interface StepsNavigateProps {
+    isFirstStep: boolean;
+    isLastStep: boolean;
+    onNext: () => void;
+    onBack: () => void;
+}
+
+const StepsNavigate: React.FC<StepsNavigateProps> = ({ isFirstStep, isLastStep, onNext, onBack }) => {
     return (
         <div className="flex justify-end gap-4 pt-12">
-            <button className="px-4 py-2 bg-black text-white rounded-3xl">Go back</button>
-            <button className="px-6 py-2 bg-black text-white rounded-3xl">Next</button>
+            <button 
+                onClick={onBack} 
+                disabled={isFirstStep} 
+                className={`px-4 py-2 bg-black ${isFirstStep ? 'opacity-60' : 'opacity-100'} text-white rounded-3xl`}
+            >
+                Go back
+            </button>
+
+            <button 
+                onClick={onNext} 
+                disabled={isLastStep} 
+                className={`px-6 py-2 bg-black ${isLastStep ? 'opacity-60' : 'opacity-100'} text-white rounded-3xl`}
+            >
+                Next
+            </button>
         </div>
     )
 }
