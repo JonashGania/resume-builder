44983be803db41898fa33a6ff1640ea975a54501
JonashGana <jonashtiglao@gmail.com>
2024-11-01 23:11:46 +0800

    initial commit

---
+++ src/interface/types.ts
@@ -0,0 +1,33 @@
+export interface PersonalInfo {
+    firstName: string, 
+    lastName: string, 
+    email: string, 
+    phone: string, 
+    address: string, 
+    city: string, 
+    postalCode: string
+}
+
+export interface EducationInfo {
+    schoolName: string, 
+    location: string, 
+    degree: string, 
+    major: string, 
+    gradMonth: string, 
+    gradYear: string,
+}
+
+export interface ExperienceInfo{
+    position: string,
+    companyName: string,
+    companyCity: string,
+    startDate: string,
+    endDate: string,
+    jobDescription: string,
+}
+
+export interface FormData{
+    personalInfo: PersonalInfo;
+    educationInfo: EducationInfo;
+    experienceInfo: ExperienceInfo;
+}
