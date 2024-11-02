8f29330d6dccb1ff5732c6866016372b54e78fde
JonashGana <jonashtiglao@gmail.com>
2024-11-02 23:32:01 +0800

    added comment

--- src/components/Preview/SkillsSection.tsx
+++ src/components/Preview/SkillsSection.tsx
@@ -1,15 +1,17 @@
 const SkillsSection = () => {
     return (
         <section className="w-full">
-            <h2 className="text-center font-bold"></h2>
+            <h2 className="text-center font-bold">Skills</h2>
             <div className="pb-4">
-                <h4 className="font-bold"></h4>
+                <h4 className="font-bold">Technical Skills</h4>
                 <ul className="list-disc pl-8">
+                    {/* {technicalSkills.map} */}
                 </ul>
             </div>
             <div>
-                <h4 className="font-bold"></h4>
+                <h4 className="font-bold">Personal Skills</h4>
                 <ul className="list-disc pl-8">
+                    {/* {personalSkills.map} */}
                 </ul>
             </div>
         </section>
