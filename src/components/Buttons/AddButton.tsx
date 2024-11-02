f15df36b8bae9cbf555d3979edffce7906c11e61
JonashGana <jonashtiglao@gmail.com>
2024-11-02 23:27:28 +0800

    added onclick props

--- src/components/Buttons/AddButton.tsx
+++ src/components/Buttons/AddButton.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 interface AddButtonProps {
     addText: string;
+    handleClick: () => void;
 }
 
-const AddButton: React.FC<AddButtonProps> = ({ addText }) => {
+const AddButton: React.FC<AddButtonProps> = ({ addText, handleClick }) => {
     return (
-        <button className="py-3 px-4 bg-black flex items-center gap-2 rounded-md mt-4">
+        <button 
+            onClick={handleClick} 
+            className="py-3 px-4 bg-black flex items-center gap-2 rounded-md mt-4"
+        >
             <svg xmlns="http://www.w3.org/2000/svg" width={15} height={15} viewBox="0 0 448 512">
                 <path fill="#ffffff" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/>
             </svg>
