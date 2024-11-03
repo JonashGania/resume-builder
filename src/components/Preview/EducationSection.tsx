53bc26e55934fe85296cb11885db8adf755d8588
JonashGana <jonashtiglao@gmail.com>
2024-11-03 23:06:58 +0800

    capitalized values

--- src/components/Preview/EducationSection.tsx
+++ src/components/Preview/EducationSection.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { EducationInfo } from "../../interface/types"
+import { capitalizeWords } from "../../helpers/utils";
 
 interface EducationSectionProps {
     educationInfo: EducationInfo;
 }
 
 const EducationSection: React.FC<EducationSectionProps> = ({ educationInfo }) => {
+    const schoolName = capitalizeWords(educationInfo.schoolName);
+    const location = capitalizeWords(educationInfo.location);
+    const degree = capitalizeWords(educationInfo.degree);
+    const major = capitalizeWords(educationInfo.major);
+
     return (
         <section className="w-full pt-8">
             <h2 className="text-center font-bold pb-2">Education</h2>
             <div className="flex justify-between">
                 <div>
-                    <h2 className="text-lg font-bold leading-5">{educationInfo.schoolName}</h2>
-                    <h4 className="leading-5">{educationInfo.degree}</h4>
-                    <h4 className="leading-5"><i>{educationInfo.major}</i></h4>
+                    <h2 className="text-lg font-bold leading-5">{schoolName}</h2>
+                    <h4 className="leading-5">{degree}</h4>
+                    <h4 className="leading-5"><i>{major}</i></h4>
                 </div>
                 <div className="text-end">
-                    <h4>{educationInfo.location}</h4>
+                    <h4>{location}</h4>
                     <h4>{educationInfo.gradMonth} {educationInfo.gradYear}</h4>
                 </div>
             </div>
