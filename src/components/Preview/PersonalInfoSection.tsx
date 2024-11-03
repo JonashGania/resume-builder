6766d92604deebdf3adeadaf19996f9467d9f118
JonashGana <jonashtiglao@gmail.com>
2024-11-03 23:08:27 +0800

    capitalize values

--- src/components/Preview/PersonalInfoSection.tsx
+++ src/components/Preview/PersonalInfoSection.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { PersonalInfo } from "../../interface/types";
+import { capitalizeWords } from "../../helpers/utils";
 
 interface PersonalInfoProps {
     personalInfo: PersonalInfo;
 }
 
 const PersonalInfoSection: React.FC<PersonalInfoProps> = ({ personalInfo }) => {
+    const firstName = capitalizeWords(personalInfo.firstName);
+    const lastName = capitalizeWords(personalInfo.lastName);
+    const address = capitalizeWords(personalInfo.address);
+    const city = capitalizeWords(personalInfo.city);
+
     return (
         <section className="w-full">
-            <h2 className="text-center text-xl border-black border-b font-bold">{personalInfo.firstName} {personalInfo.lastName}</h2>
+            <h2 className="text-center text-xl border-black border-b font-bold">{firstName} {lastName}</h2>
             <div className="text-center">
-                <span>{personalInfo.address} </span>
-                <span>• {personalInfo.city}, {personalInfo.postalCode} </span>
+                <span>{address} </span>
+                <span>• {city}, {personalInfo.postalCode} </span>
                 <span>• {personalInfo.email} </span>
                 <span>• {personalInfo.phone}</span>
             </div>
