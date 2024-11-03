9b4da706ca4032902551be07ddf268b3da48e49a
JonashGana <jonashtiglao@gmail.com>
2024-11-03 23:11:46 +0800

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className=' text-5xl font-bold pb-2'>Resume Builder</h1>
+            <p className=' text-slate-600'>Create your own brand new resume in less than 5 minutes.</p>
+        </header>
+    )
+}
+
+export default Header
