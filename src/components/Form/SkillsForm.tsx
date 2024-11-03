4d9927112aa2ade982b486b26c4e99f995387d65
JonashGana <jonashtiglao@gmail.com>
2024-11-03 23:06:00 +0800

    refactored used Skills Context

--- src/components/Form/SkillsForm.tsx
+++ src/components/Form/SkillsForm.tsx
@@ -1,48 +1,20 @@
-import React, { useState } from "react"
 import AddButton from "../Buttons/AddButton"
-import { SkillsInfo } from "../../interface/types"
+import { useSkills } from "../../context/skillsContext"
 
-interface SkillsFormProps {
-    skillsData: SkillsInfo
-}
-
-const SkillsForm: React.FC<SkillsFormProps> = ({ skillsData }) => {
-    const [inputsTS, setInputsTS] = useState(skillsData.technicalSkills);
-    const [inputsPS, setInputsPS] = useState(skillsData.personalSkills);
-
-    const inputChangeTS = (index: number, value: string) => {
-        const newValues = [...inputsTS];
-        newValues[index] = value;
-        setInputsTS(newValues);
-    }
-
-    const inputChangePS = (index: number, value: string) => {
-        const newValues = [...inputsPS];
-        newValues[index] = value;
-        setInputsPS(newValues);
-    }
-
-    const handleAddTSInputs = () => {
-        setInputsTS([...inputsTS, '']);
-    }
-
-    const handleAddPSInputs = () => {
-        setInputsPS([...inputsPS, '']);
-    }
-
-    console.log(inputsPS)
+const SkillsForm = () => {
+    const { technicalSkills, personalSkills, handleTechnicalSkills, handlePersonalSkills, addTechnicalSkills, addPersonalSkills } = useSkills()
 
     return (
         <div>
             <div>
                 <h2 className="text-xl font-bold">Techinical Skills</h2>
                 <div className="flex flex-col gap-2">
-                    {inputsTS.map((value, index)  =>(
+                    {technicalSkills.map((value, index)  =>(
                         <input
                             key={index} 
                             type="text"
                             value={value}
-                            onChange={(e) => inputChangeTS(index, e.target.value)} 
+                            onChange={(e) => handleTechnicalSkills(index, e.target.value)} 
                             placeholder="Skills" 
                             className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                         />
@@ -51,19 +23,19 @@ const SkillsForm: React.FC<SkillsFormProps> = ({ skillsData }) => {
 
                 <AddButton 
                     addText="Skills" 
-                    handleClick={handleAddTSInputs}
+                    handleClick={addTechnicalSkills}
                 />
             </div>
            
             <div className="pt-12">
                 <h2 className="text-xl font-bold">Personal Skills</h2>
                 <div className="flex flex-col gap-2">
-                    {inputsPS.map((value,index) => (
+                    {personalSkills.map((value,index) => (
                         <input
                             key={index} 
                             type="text"
                             value={value}
-                            onChange={(e) => inputChangePS(index, e.target.value)} 
+                            onChange={(e) => handlePersonalSkills(index, e.target.value)} 
                             placeholder="Skills" 
                             className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                         />
@@ -71,7 +43,7 @@ const SkillsForm: React.FC<SkillsFormProps> = ({ skillsData }) => {
                 </div>
                 <AddButton 
                     addText="Skills" 
-                    handleClick={handleAddPSInputs}
+                    handleClick={addPersonalSkills}
                 />
             </div>
         </div>
