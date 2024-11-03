9485bf9746c38c1b58dd8b8aa9c52fc12713ae64
JonashGana <jonashtiglao@gmail.com>
2024-11-03 23:03:57 +0800

    added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import ResumeBuilder from './components/ResumeBuilder'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className='px-12 pt-12 bg-gray-200 w-full min-h-screen'>
-      <h1 className=' text-5xl font-bold pb-2'>Resume Builder</h1>
-      <p className=' text-slate-600'>Create your own brand new resume in less than 5 minutes.</p>
+      <Header />
       <ResumeBuilder />
       <Footer />
     </div>
