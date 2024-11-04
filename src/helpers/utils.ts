084ebb72ff85ca12a1565620345015e481716f54
JonashGana <jonashtiglao@gmail.com>
2024-11-04 21:10:29 +0800

    added handleDownloadPDF function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,6 @@
 import {parse, format, isValid} from 'date-fns'
+import html2pdf from 'html2pdf.js'
+import { FormData } from '../interface/types';
 
 export const formattedDate = (date: string) => {
     if (date) {
@@ -21,3 +23,14 @@ export const capitalizeWords = (text: string) => {
 
 }
      
+export const handleDownloadPDF = (resumeRef: React.RefObject<HTMLDivElement>, formData: FormData) => {
+    const element = resumeRef.current;
+    const options = {
+        margin: 0,
+        filename: `${formData.personalInfo.lastName ? formData.personalInfo.lastName : "my"}_resume.pdf`,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait'},
+    };
+    html2pdf().set(options).from(element).save();
+}
