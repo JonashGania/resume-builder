ca7600c6945ba216be63e77d6fb41163f592fd48
JonashGana <jonashtiglao@gmail.com>
2024-11-05 21:37:26 +0800

    changed text center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-        <header>
-            <h1 className=' text-5xl font-bold pb-2'>Resume Builder</h1>
+        <header className="laptop:text-start text-center">
+            <h1 className='text-3xl mobile:text-5xl font-bold pb-2'>Resume Builder</h1>
             <p className=' text-slate-600'>Create your own brand new resume in less than 5 minutes.</p>
         </header>
     )
