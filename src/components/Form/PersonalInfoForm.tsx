e4b804ff743bb86e48dfdc3713be3d95ec421078
JonashGana <jonashtiglao@gmail.com>
2024-11-05 21:36:51 +0800

    added responsive styles

--- src/components/Form/PersonalInfoForm.tsx
+++ src/components/Form/PersonalInfoForm.tsx
@@ -13,8 +13,8 @@ const PersonalInfoForm: React.FC<PersonalInfoProps> = ({ data, onChange }) => {
         <div>
             <h2 className="text-xl font-bold pb-4">Personal Information</h2>
             <div className="flex flex-col gap-4">
-                <div className="flex w-full gap-4">
-                    <div className="w-1/2">
+                <div className="flex w-full gap-4 flex-col mobile:flex-row">
+                    <div className="mobile:w-1/2 w-full">
                         <label htmlFor="firstName" className=" text-slate-600">First Name</label>
                         <input 
                             type="text"
@@ -24,7 +24,7 @@ const PersonalInfoForm: React.FC<PersonalInfoProps> = ({ data, onChange }) => {
                             className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                         />
                     </div>
-                    <div className="w-1/2">
+                    <div className="mobile:w-1/2 w-full">
                         <label htmlFor="lastName" className=" text-slate-600">Last Name</label>
                         <input 
                             type="text" 
@@ -58,8 +58,8 @@ const PersonalInfoForm: React.FC<PersonalInfoProps> = ({ data, onChange }) => {
                         className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                     />
                 </div>
-                <div className="flex w-full gap-4">
-                    <div className="w-1/2">
+                <div className="flex w-full gap-4 flex-col mobile:flex-row">
+                    <div className="mobile:w-1/2 w-full">
                         <label htmlFor="address" className="text-slate-600">Address</label>
                         <input 
                             type="text" 
@@ -69,8 +69,8 @@ const PersonalInfoForm: React.FC<PersonalInfoProps> = ({ data, onChange }) => {
                             className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                         />
                     </div>
-                    <div className="w-1/2 flex gap-2">
-                        <div className="w-1/2">
+                    <div className="mobile:w-1/2 w-full flex gap-2">
+                        <div className="mobile:w-1/2 w-full">
                             <label htmlFor="city" className="text-slate-600">City</label>
                             <input 
                                 type="text" 
@@ -80,7 +80,7 @@ const PersonalInfoForm: React.FC<PersonalInfoProps> = ({ data, onChange }) => {
                                 className="w-full mt-1 border rounded-sm border-gray-300 px-2 py-2 outline-none text-slate-600 focus:border-slate-600 text-ellipsis"
                             />
                         </div>
-                        <div className="w-1/2">
+                        <div className="mobile:w-1/2 w-full">
                             <label htmlFor="postal-code" className="text-slate-600">Postal Code</label>
                             <input 
                                 type="text" 
