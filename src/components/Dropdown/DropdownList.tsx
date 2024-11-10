4e777e97d992a868a9dafa98486bb5967de5ede6
JonashGana <jonashtiglao@gmail.com>
2024-11-10 22:06:30 +0800

    changed dropdown list conditional rendering

--- src/components/Dropdown/DropdownList.tsx
+++ src/components/Dropdown/DropdownList.tsx
@@ -8,17 +8,22 @@ interface DropdownListInt {
 
 const DropdownList: React.FC<DropdownListInt> = ({ options, selectOption, menuShow }) => {
     return (
-        <ul className={`dropdown-list ${menuShow ? 'flex' : 'hidden'} w-full absolute mt-1 right-0 flex-col z-10 bg-white px-2 py-2 max-h-[200px] overflow-y-scroll`}>
-            {options.map((item, index) => (
-                <li 
-                    key={index} 
-                    onClick={() => selectOption(item)}
-                    className="py-1 hover:font-semibold cursor-pointer text-slate-600 hover:text-black"
-                >
-                    {item}
-                </li>
-            ))}
-        </ul>
+        <>
+            {menuShow && (
+                <ul className={`dropdown-list flex w-full absolute mt-1 right-0 flex-col z-10 bg-white px-2 py-2 max-h-[200px] overflow-y-scroll`}>
+                    {options.map((item, index) => (
+                        <li 
+                            key={index}
+                            data-testid="dropdown-item" 
+                            onClick={() => selectOption(item)}
+                            className="py-1 hover:font-semibold cursor-pointer text-slate-600 hover:text-black"
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
     )
 }
 
