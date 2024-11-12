04b77e70faae7a6cb1d1bb0c532c7c9d355966b9
JonashGana <jonashtiglao@gmail.com>
2024-11-12 22:24:56 +0800

    added data-testid

--- src/components/Section/ExperienceSection.tsx
+++ src/components/Section/ExperienceSection.tsx
@@ -5,7 +5,7 @@ const ExperienceSection = () => {
     const { experiences, currentExperience } = useExperience()
 
     return (
-        <section className="w-full pt-8">
+        <section data-testid="experience-section" className="w-full pt-8">
             <h2 className="text-center font-bold pb-2">Experience</h2>
             {experiences.map((experience, index) => (
                 <MapExperiences experienceInfo={experience} key={index}/>
