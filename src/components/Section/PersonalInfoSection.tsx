0c91d14845b5eaf2518e7ec9f1e47ca3da575f42
JonashGana <jonashtiglao@gmail.com>
2024-11-12 22:25:03 +0800

    added data-testid

--- src/components/Section/PersonalInfoSection.tsx
+++ src/components/Section/PersonalInfoSection.tsx
@@ -13,7 +13,7 @@ const PersonalInfoSection: React.FC<PersonalInfoProps> = ({ personalInfo }) => {
     const city = capitalizeWords(personalInfo.city);
 
     return (
-        <section className="w-full">
+        <section data-testid="personalInfo-section" className="w-full">
             <div className="border-b border-black">
                 <h2 className="text-3xl font-bold pb-2">{firstName} {lastName}</h2>
                 <div className="flex flex-col leading-5">
