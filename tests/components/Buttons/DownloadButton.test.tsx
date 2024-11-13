90744ea64015cf07b58f2063b51a6f4a7d5b2447
JonashGana <jonashtiglao@gmail.com>
2024-11-13 21:16:21 +0800

    refactor unit test

--- tests/components/Buttons/DownloadButton.test.tsx
+++ tests/components/Buttons/DownloadButton.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import DownloadButton from '../../../src/components/Buttons/DownloadButton'
 import userEvent from '@testing-library/user-event'
-import { handleDownloadPDF } from '../../../src/helpers/utils'
 
 vi.mock('../../../src/helpers/utils', () => ({
     handleDownloadPDF: vi.fn(),
@@ -11,18 +10,18 @@ describe('Download Button', () => {
     it('should render button text', () => {
         render(<DownloadButton formData={{}} resumeRef={{ current: null }}/>)
 
-        expect(screen.getByText(/Download/i)).toBeInTheDocument();
+        expect(screen.getByRole('button', {name: /Download/i})).toBeInTheDocument();
     })
 
     it('should expect to call handleDownloadPDF when button is clicked', async () => {
         const { handleDownloadPDF } = await import('../../../src/helpers/utils');
+        const user = userEvent.setup();
 
         render(<DownloadButton formData={{}} resumeRef={{ current: null }} />);
 
         const button = screen.getByRole('button');
-        const user = userEvent.setup();
         await user.click(button);
         
-        expect(handleDownloadPDF).toHaveBeenCalledTimes(1);
+        expect(handleDownloadPDF).toHaveBeenCalled();
     })
 })
