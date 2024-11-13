259847fa51eaa0e58a8f6e821e8fe8cda97d8b08
JonashGana <jonashtiglao@gmail.com>
2024-11-13 21:16:14 +0800

    refactor unit test

--- tests/components/Buttons/AddButton.test.tsx
+++ tests/components/Buttons/AddButton.test.tsx
@@ -4,18 +4,19 @@ import userEvent from '@testing-library/user-event'
 
 describe('Add Button', () => {
     it('should render add button with custom text', () => {
-        render(<AddButton addText='Skills'/>)
+        const handleClick = vi.fn();
+
+        render(<AddButton addText='Skills' handleClick={handleClick}/>)
 
         expect(screen.getByText(/Add Skills/i)).toBeInTheDocument();
     })
 
     it('should call handleClick when the button is clicked', async () => {
         const handleClick = vi.fn();
-
-        render(<AddButton handleClick={handleClick}/>)
-
-        const button = screen.getByRole('button');
         const user = userEvent.setup();
+        render(<AddButton addText='Skills' handleClick={handleClick}/>)
+
+        const button = screen.getByRole('button', {name: /Add Skills/i});
         await user.click(button);
 
         expect(handleClick).toHaveBeenCalledTimes(1);
