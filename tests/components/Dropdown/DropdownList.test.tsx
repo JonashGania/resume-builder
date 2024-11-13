2dd685d6027f3b883dd41c046ba2d6ccb3eac5c1
JonashGana <jonashtiglao@gmail.com>
2024-11-13 21:17:43 +0800

    refactor unit test

--- tests/components/Dropdown/DropdownList.test.tsx
+++ tests/components/Dropdown/DropdownList.test.tsx
@@ -2,11 +2,10 @@ import { render, screen } from '@testing-library/react'
 import DropdownList from '../../../src/components/Dropdown/DropdownList'
 import userEvent from '@testing-library/user-event';
 
+const mockSelectOption = vi.fn();
+const itemOptions = ['item1', 'item2', 'item3', 'item4']
 describe('DropdownList', () => {
-    const mockSelectOption = vi.fn();
-    const itemOptions = ['item1', 'item2', 'item3', 'item4']
-
-    it('should render options when menuShow is true', () => {
+    it('should render option lists when menuShow is true', () => {
         render(<DropdownList options={itemOptions} selectOption={mockSelectOption} menuShow={true}/>)
 
         const itemList = screen.getAllByTestId('dropdown-item');
@@ -28,13 +27,13 @@ describe('DropdownList', () => {
     })
 
     it('should call selectOption with the correct item when an option is clicked', async () => {
-        render(<DropdownList options={itemOptions} selectOption={mockSelectOption} menuShow={true}/>)
-        const optionList = screen.getByText('item1');
         const user = userEvent.setup();
+        render(<DropdownList options={itemOptions} selectOption={mockSelectOption} menuShow={true}/>)
 
+        const optionList = screen.getByText('item1');
         await user.click(optionList);
 
-        expect(mockSelectOption).toHaveBeenCalledTimes(1);
+        expect(mockSelectOption).toHaveBeenCalled();
         expect(mockSelectOption).toHaveBeenCalledWith('item1');
     })
 })
